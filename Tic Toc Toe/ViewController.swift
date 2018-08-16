ceea540c4b787e23643c2da9ecac0c7fd65f179b
WITZ <votuhai@gmail.com>
2018-08-16 15:01:16 +0700

    Initial Commit

---
+++ Tic Toc Toe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tic Toc Toe
+//
+//  Created by WITZ on 8/16/18.
+//  Copyright © 2018 WITZ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

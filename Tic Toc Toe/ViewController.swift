8a90f308e7a419048f917b6f2101040d9caf150c
WITZ <votuhai@gmail.com>
2018-08-21 11:16:35 +0700

    game using 2 d array

--- Tic Toc Toe/ViewController.swift
+++ Tic Toc Toe/ViewController.swift
@@ -9,17 +9,103 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var player = 1
+    var playerWin = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+    
+    @IBOutlet var buttons: [UIButton]!
+    @IBOutlet weak var labelMsg: UILabel!
+    
+    func starGame(){ //
+        playerWin = [[0, 0, 0] , [0, 0, 0], [0, 0, 0]]
+        for button in buttons{
+            button.setTitle(nil, for: UIControlState.normal)
+        }
+        player = 1
+    }
+    
+    @IBAction func selectButton(_ sender: UIButton) {
+        
+        let rowIndex = sender.tag / 3
+        let colIndex = sender.tag % 3
+        
+        if playerWin[rowIndex][colIndex] != 0 {
+            return
+        }
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        playerWin[rowIndex][colIndex] = player
+        
+        if player == 1 {
+            sender.setTitle("O", for: UIControlState.normal)
+            player += 1
+        } else if player == 2{
+            sender.setTitle("X", for: UIControlState.normal)
+            sender.setTitleColor(.red, for: UIControlState.normal)
+            player = 1
+        }
+        
+        let winner = thisNotWinner()
+        
+        switch winner {
+        case 0:
+            player = (player % 2) + 1
+        case 1:
+            labelMsg.text = "Player 1 is the winner!"
+
+        case 2:
+            labelMsg.text = "Player 2 is the winner!"
+           
+        default:
+            labelMsg.text = "Game Stoped, We don't have Winner"
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func playAgain(_ sender: UIButton) {
+        starGame()
     }
-
-
+    
+    func thisNotWinner() -> Int {
+        
+        if playerWin[0][0] != 0 && playerWin[0][0] == playerWin[0][1] && playerWin[0][1] == playerWin[0][2] {
+            return playerWin[0][0]
+        }
+        
+        
+        if playerWin[1][0] != 0 && playerWin[1][0] == playerWin[1][1] && playerWin[1][1] == playerWin[1][2] {
+            return playerWin[1][0]
+        }
+        
+        if playerWin[2][0] != 0 && playerWin[2][0] == playerWin[2][1] && playerWin[2][1] == playerWin[2][2] {
+            return playerWin[2][0]
+        }
+        
+        if playerWin[0][0] != 0 && playerWin[0][0] == playerWin[1][0] && playerWin[1][0] == playerWin[2][0] {
+            return playerWin[0][0]
+        }
+        
+        if playerWin[0][1] != 0 && playerWin[0][1] == playerWin[1][1] && playerWin[1][1] == playerWin[2][1] {
+            return playerWin[0][1]
+        }
+        
+        if playerWin[0][2] != 0 && playerWin[0][2] == playerWin[1][2] && playerWin[1][2] == playerWin[2][2] {
+            return playerWin[2][2]
+        }
+        
+        if playerWin[0][2] != 0 && playerWin[0][2] == playerWin [1][1] && playerWin[1][1] == playerWin[2][0] {
+            return playerWin[2][0]
+        }
+        
+        if playerWin[0][0] != 0 && playerWin[0][0] == playerWin[1][1] && playerWin[1][1] == playerWin[2][2]{
+            return playerWin[2][2]
+        }
+        return 0
+    }
 }
+
+
+// - Có bao nhiêu cách để unwrap 1 biến optional.
+// - Dùng `as?` thay vì `as!`
+// - Win
+//
+
 
